94cd8dbbcfbefc744204f81245e64d799fdf1db0
Chris Conley <pair+chris@realscout.com>
2015-07-16 10:49:28 -0700

    adds first files with webpack and babel

---
+++ debugger/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel?stage=1'
+          }
+        ]
+    }
+};
