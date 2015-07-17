ba40b113b9b4720aeeb9f39000339c6121e9c441
Chris Conley <pair+chris@realscout.com>
2015-07-17 11:54:02 -0700

    starts emulator data structure

--- server/src/emulator.rs
+++ server/src/emulator.rs
@@ -4,8 +4,12 @@ use rustc_serialize::json;
 // implementations
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct Emulator {
-    stack: String,
-    registers: Vec<u8>,
+    dataSpace: String,
+    registerFile: Vec<u8>,
+    ioMemory: Vec<u8>,
+    program: Vec<u8>,
+    stack: u16,
+    stackPointer: u16
 }
 
 pub fn serialize() -> String {
@@ -17,6 +21,16 @@ pub fn serialize() -> String {
   // Serialize using `json::encode`
   json::encode(&object).unwrap()
 }
+//#[test]
+//fn can_add() {
+  //let emulator = Emulator {
+    //registerFile: vec![0,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
+  //};
+  //let operation = "add r1,r2";
+  //let result = add(emulator, "r1,r2");
+  //assert_eq!(5, emulator.registerFile[1]);
+  //assert_eq!(3, emulator.registerFile[2]);
+//}
 
 #[test]
 fn it_works() {
