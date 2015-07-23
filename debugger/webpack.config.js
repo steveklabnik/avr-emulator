5cdea7570103b3e79289b6717e7f81bba74c210b
Davide Curletti <curlettid@gmail.com>
2015-07-23 00:48:52 -0700

    Adds hot scss reloading

--- debugger/webpack.config.js
+++ debugger/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
           },
           {
             test: /\.scss$/,
-            loader: "style!css!sass"
+            loader: 'style!css!sass'
           }
         ]
     },
