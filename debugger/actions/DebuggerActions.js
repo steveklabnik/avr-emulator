7b64e5b663d10202864a3a2f25882e47fccae88e
Davide Curletti <curlettid@gmail.com>
2015-07-23 21:01:15 -0700

    Adds step loop outside of redux flow

--- debugger/actions/DebuggerActions.js
+++ debugger/actions/DebuggerActions.js
@@ -4,6 +4,7 @@ export const WEBSOCKET_REQUEST = 'websocketRequest';
 export const WEBSOCKET_UPDATE = 'websocketUpdate';
 
 export const PERFORM_STEP = 'performStep';
+export const EXECUTE_PROGRAM = 'executeProgram';
 
 export function performStep() {
   return {
@@ -11,4 +12,22 @@ export function performStep() {
     socketMessageName: PERFORM_STEP,
     socketMessage: {}
   };
+}
+
+export function executeProgram() {
+  // return dispatch => {
+  //   setTimeout(() => {
+  //     dispatch(performStep());
+  //   }, 1000);
+  // };
+
+   //  return (dispatch, getState) => {
+	  //   // const { timerId } = getState();
+	  //   timerId = undefined	
+	  //     const timerId = setInterval(() => {
+	  //       dispatch(performStep())
+	  //     }, 1000)
+	  //     dispatch({type: "Nothing", payload: timerId}) // a store supposed to save `timerId`
+	  //   }
+	  // }
 }
--- debugger/components/DebuggerButtons.js
+++ debugger/components/DebuggerButtons.js
@@ -3,18 +3,27 @@ import React, { PropTypes } from 'react';
 export default class DebuggerButtons {
 
   render() {
-    const { performStep } = this.props;
+    const { performStep, executeProgram } = this.props;
+
+    let testProgram = function () {
+      setInterval(() => {
+        performStep()
+      }, 1000)
+    }
+
     return (
       <div>
         <h2>Debugger Actions</h2>
-        <p>
+        <div className='controls'>
           <button onClick={performStep}>Step</button>
-        </p>
+          <button onClick={testProgram}>Execute Program</button>
+        </div>
       </div>
     );
   }
 }
 DebuggerButtons.propTypes = {
   performStep: PropTypes.func.isRequired,
+  executeProgram: PropTypes.func.isRequired,
   emulator: PropTypes.object.isRequired
 };
