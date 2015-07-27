2ebbe78e5c7c24b23486b00cdfbaf051d00cec88
Chris Conley <pair+chris@realscout.com>
2015-07-27 11:54:34 -0700

    styling tweaks

--- debugger/containers/EmulatorApp.js
+++ debugger/containers/EmulatorApp.js
@@ -30,19 +30,18 @@ export default class EmulatorApp {
                 <p>
                   Inspired by&nbsp;
                   <a href="http://sockpuppet.org/blog/2015/07/13/starfighter/">
-                    Starfighters Summer 2015 announcement
+                    Starfighter's Summer 2015 announcement
                   </a>,
                   we thought that implementing our own&nbsp;
                   <a href="http://www.atmel.com/images/atmel-0856-avr-instruction-set-manual.pdf">
                     Atmel AVR 8-bit
                   </a>
                   &nbsp;emulator would be a pretty fun problem to tackle.
-                  During our last&nbsp;
+                  Built with React.js, Redux.js, Rust and Docker
+                  during our last&nbsp;
                   <a href="https://twitter.com/chrisconley/status/618830194971774976">
                     biweekly hack day
-                  </a>
-                  &nbsp;we dove in with a few technologies we have had our eyes on
-                  and pushed out the first version.
+                  </a>.
                 </p>
               </div>
             </div>
